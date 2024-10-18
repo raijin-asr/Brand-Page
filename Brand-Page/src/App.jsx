6250e4f964e23d5f1c0be34f98ea650d7669f840
Ameer Sampang Rai <97660344+raijin-asr@users.noreply.github.com>
2024-10-18 10:55:29 +0545

    Day 3: Add Nav and body div to app.jsx, and style to app.css

--- Brand-Page/src/App.jsx
+++ Brand-Page/src/App.jsx
@@ -1,14 +1,50 @@
 import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      Hello Ameer!
+      <nav>
+        <div classname="logo">
+          <img src="/vite.svg" alt="Logo" />
+        </div>
+        <div classname="menu">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+        <button>Login</button>
+      </nav>
+      <main className="bodyDiv container">
+        <div className="bodyDiv-content">
+          <h1>AMEER RAI</h1>
+          <p>
+            Welcome to my Brand Page. I am happy to see you here. I am a
+          </p>
+
+          <div className="bodyDiv-btn">
+            <button>Read More </button>
+            <button className="secondary-btn">Donate</button>
+          </div>
+
+          <div className="socailDiv">
+            <p>My Social Media</p>
+
+            <div className="social-icons">
+              <img src="/vite.svg" alt="fb-logo" />
+              <img src="/vite.svg" alt="linkedin-logo" />
+            </div>
+          </div>
+        </div>
+        <div className="bodyDiv-image">
+          <img src="/vite.svg" alt="bodyDiv-image" />
+        </div>
+      </main>
     </>
   )
 }
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Brand Page Project using React
 # Daywise Timeline Project Development:
     Day 1: Project Setup, installation using vite in react
     Day 2: Execute code and Filter default code
+    Day 3: Add Nav and body div to app.jsx, and style to app.css
